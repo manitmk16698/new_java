2e3ee2aa571449464ee5102c5a2f5214c844ca7a
MANIKANTA THIRUKKOVELA <manikanta16698@gmail.com>
2025-01-26 00:10:04 +0530

    Added

---
+++ JAVA/Identifiers.java
@@ -0,0 +1,20 @@
+package JAVA;                                    //JAVA is a identifier of pakage 
+
+public class Identifiers                        //Identifies is an identifier of class
+
+{
+    public static void main(String[] args) {     //main is a method name ,String is a predefined class name and args is a vaiable name
+        
+    }    
+}
+
+class test                                          //test
+{
+    int age =27;                                    //age
+
+    public void count()                             //count
+    {
+        System.out.println();
+    }
+}
+
